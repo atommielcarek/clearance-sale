987bab67eaf5af4867109b588af0e4d8ab0d8263
Prometheus <atommielcarek@gmail.com>
2021-10-27 11:30:45 -0600

    final product

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//All incoming request objects will be recognized as json objects
 app.use(express.json());
+//inbuilt in express to recognize the incoming Request Object as strings or arrays.
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
